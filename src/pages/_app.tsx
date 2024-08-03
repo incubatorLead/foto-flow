d8a9e65a1209697fb8e1b4b2232989b8ec3bcea8
Abrosimov96 <Abrosimov0423@gmail.com>
2024-08-03 13:05:46 +0300

    init: redux store and provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,34 @@
 import type { AppProps } from 'next/app'
+
 // ToDo fix css import ESLint: Missing file extension "css" for "@teamlead.incubator/ui-kit/css"(import/extensions)
+import { ReactNode } from 'react'
+import { Provider } from 'react-redux'
+
+import { wrapper } from '@/store'
+import { Toaster } from 'sonner'
+
 // eslint-disable-next-line import/extensions
 import '@teamlead.incubator/ui-kit/css'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, ...rest }: AppProps) {
+  const { props, store } = wrapper.useWrappedStore(rest)
+
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Toaster />
+        <Component {...props.pageProps} />
+      </Layout>
+    </Provider>
+  )
+}
+
+function Layout({ children }: { children: ReactNode }) {
+  return (
+    <div>
+      <header>header</header>
+      <main>{children}</main>
+      <footer>footer</footer>
+    </div>
+  )
 }
