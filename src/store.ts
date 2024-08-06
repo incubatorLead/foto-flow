3606afeb7643a9c76b0460cb4d1fdfff974da41d
Vitalii <vitalii.hryshko.rss@gmail.com>
2024-08-06 12:57:45 +0200

    IN-33 feat: start sign in form

--- src/store.ts
+++ src/store.ts
@@ -1,13 +1,16 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux"
 
+import { service } from "@/services/services"
 import { Action, ThunkAction, configureStore } from "@reduxjs/toolkit"
 import { createWrapper } from "next-redux-wrapper"
 
 const makeStore = () =>
   configureStore({
     devTools: true,
-    middleware: getDefaultMiddleware => getDefaultMiddleware(),
-    reducer: {}
+    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(service.middleware),
+    reducer: {
+      [service.reducerPath]: service.reducer
+    }
   })
 
 export type AppStore = ReturnType<typeof makeStore>
